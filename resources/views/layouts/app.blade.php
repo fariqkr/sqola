9a20733f1b9e03c49946e0fd109bc85406f8a6fe
FtrR <bytenauts@gmail.com>
2020-11-23 12:11:05 +0700

    Landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,32 +12,35 @@
 <body class="bg-white">
 
     {{-- Navigasi --}}
-    <nav class="mx-auto py-6 bg-white flex justify-between border-b-4 border-gray-300">
+    <nav class="py-6 bg-white border-b-4 border-gray-300">
+        <div class="container mx-auto flex justify-between">
         <ul class="flex items-center">
             <li>
-                <a href="/" class="py-3 px-14"> Sqola </a>
+                <a href="/"> 
+                    <img src="{{asset('img/logo.PNG')}}" alt="Sqola" style="height: 45px;" class="mx-8">
+                </a>
             </li>
             <li>
                 <a href="#home" class="p-3 text-gray-300 hover:text-black hover:underline"> Home </a>
             </li>
             <li>
-                <a href="#" class="p-3 text-gray-300 hover:text-black hover:underline"> Categories </a>
+                <a href="#categories" class="p-3 text-gray-300 hover:text-black hover:underline"> Categories </a>
             </li>
             <li>
-                <a href="#" class="p-3 text-gray-300 hover:text-black hover:underline"> Testimonial </a>
+                <a href="#testimonial" class="p-3 text-gray-300 hover:text-black hover:underline"> Testimonial </a>
             </li>
             <li>
-                <a href="#" class="p-3 text-gray-300 hover:text-black hover:underline"> Contact Us </a>
+                <a href="#contact" class="p-3 text-gray-300 hover:text-black hover:underline"> Contact Us </a>
             </li>
         </ul>
 
         <ul class="flex items-center">
             @auth
                 <li>
-                    <a href="" class="p-3"> Teacher Name </a>
+                    <a href="/" class="p-3 mx-1 text-blue-500 font-bold hover:text-blue-700"> Teacher Name </a>
                 </li>
                 <li>
-                    <form action="#" method="post" class="inline p-3">
+                    <form action="/" method="post" class="inline p-3 text-white bg-blue-500 rounded hover:bg-blue-700">
                         @csrf
                         <button type="submit">Logout</button>
                     </form>
@@ -45,32 +48,17 @@
             @endauth
             @guest
                 <li>
-                <a href="#" class="p-3"> Become a Teacher </a>
+                <a href="/" class="p-3 mx-1 text-blue-500 font-bold hover:text-blue-700"> Become a Teacher </a>
                 </li>
                 <li>
-                    <a href="#" class="p-3"> Login / Signup </a>
+                    <a href="/" class="p-3 text-white bg-blue-500 rounded hover:bg-blue-700"> Login / Signup </a>
                 </li>
             @endguest
         </ul>
+        </div>
     </nav>
 
-    {{-- Home --}}
-    <section class="container pt-8 md:pt-8 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center" id="home">	
-		<!--Left-->
-		<div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
-			<h1 class="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left">Get Free Education</h1>
-			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">Get Free educational and high quality education system to help you advance in your study at highschool period.</p>
-            <button class="p-4 bg-blue-800 text-white rounded-lg">
-                <a href="#" >Sign Up Now</a>
-            </button>
-		</div>
-		
-		<!--Right-->
-		<div class="w-full xl:w-3/5 py-6 overflow-y-hidden">
-			<img class="w-4/6 mx-auto lg:mr-0" src="http://www.pngall.com/wp-content/uploads/5/Learning-PNG-Free-Download.png">
-		</div>	
-    </section>
-
+    @yield('content')
 </body>
 
 </html>
