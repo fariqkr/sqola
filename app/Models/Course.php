8935d7d473b53276ce5941972de1460d7d136697
FtrR <bytenauts@gmail.com>
2020-12-09 00:04:03 +0700

    Populate course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -68,6 +68,92 @@ class Course extends Model
             'grade' => '10',
             'category' => 'fisika'
         ]);
+        
+        // Kelas 11 Fisika
+        $this->create([
+            'course_name' => 'Fluida Statis',
+            'grade' => '11',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Fluida Dinamis',
+            'grade' => '11',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Teori Kinetik Gas',
+            'grade' => '11',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Termodinamika',
+            'grade' => '11',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Gelombang Bunyi',
+            'grade' => '11',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Gelombang Cahaya',
+            'grade' => '11',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Cermin dan Lensa',
+            'grade' => '11',
+            'category' => 'fisika'
+        ]);
+
+        // Kelas 12 Fisika
+        $this->create([
+            'course_name' => 'Listrik Statis',
+            'grade' => '12',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Magnet',
+            'grade' => '12',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Fisika Kuantum',
+            'grade' => '12',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Gelombang Elektromagnetik',
+            'grade' => '12',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Listrik Arus Searah',
+            'grade' => '12',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Listrik Bolak-balik',
+            'grade' => '12',
+            'category' => 'fisika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Induksi',
+            'grade' => '12',
+            'category' => 'fisika'
+        ]);
 
         // Kelas 10 Matematika
 
@@ -104,6 +190,82 @@ class Course extends Model
         $this->create([
             'course_name' => 'Persamaan Kuadrat',
             'grade' => '10',
+            'category' => 'matematika'
+        ]);
+
+        // Kelas 11 Matematika
+
+        $this->create([
+            'course_name' => 'Logika Matematika',
+            'grade' => '11',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Matriks Dasar',
+            'grade' => '11',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Barisan dan Deret',
+            'grade' => '11',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Limit Aljabar',
+            'grade' => '11',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Turunan Aljabar',
+            'grade' => '11',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Trigonometri',
+            'grade' => '11',
+            'category' => 'matematika'
+        ]);
+
+        // Kelas 12 Matematika
+
+        $this->create([
+            'course_name' => 'Geometri Bidang Datar',
+            'grade' => '12',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Geometri Bidang Ruang',
+            'grade' => '12',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Mean, Median, Modus',
+            'grade' => '12',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Kuartil, Desil, Simpangan Baku, Varian ',
+            'grade' => '12',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Kaidah Pencacahan, Peluang, Permutasi, dan Kombinasi ',
+            'grade' => '12',
+            'category' => 'matematika'
+        ]);
+
+        $this->create([
+            'course_name' => 'Limit Fungsi Trigonometri',
+            'grade' => '12',
             'category' => 'matematika'
         ]);
 
@@ -151,6 +313,58 @@ class Course extends Model
             'category' => 'kimia'
         ]);
 
+        // Kelas 11 Kimia
+
+        $this->create([
+            'course_name' => 'Termokimia',
+            'grade' => '11',
+            'category' => 'kimia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Larutan Asam Basa',
+            'grade' => '11',
+            'category' => 'kimia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Stoikiometri',
+            'grade' => '11',
+            'category' => 'kimia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Kelarutan',
+            'grade' => '11',
+            'category' => 'kimia'
+        ]);
+
+        // Kelas 12 Kimia
+
+        $this->create([
+            'course_name' => 'Kimia Unsur',
+            'grade' => '12',
+            'category' => 'kimia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Radioaktif',
+            'grade' => '12',
+            'category' => 'kimia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Senyawa Karbon',
+            'grade' => '12',
+            'category' => 'kimia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Elektrokimia',
+            'grade' => '12',
+            'category' => 'kimia'
+        ]);
+
         // Kelas 10 Biologi
 
         $this->create([
@@ -182,6 +396,59 @@ class Course extends Model
             'grade' => '10',
             'category' => 'biologi'
         ]);
+
+        // Kelas 11 Biologi
+
+        $this->create([
+            'course_name' => 'Biokimia',
+            'grade' => '11',
+            'category' => 'biologi'
+        ]);
+
+        $this->create([
+            'course_name' => 'Sel',
+            'grade' => '11',
+            'category' => 'biologi'
+        ]);
+
+        $this->create([
+            'course_name' => 'Jaringan Tumbuhan',
+            'grade' => '11',
+            'category' => 'biologi'
+        ]);
+
+        $this->create([
+            'course_name' => 'Jaringan Hewan',
+            'grade' => '11',
+            'category' => 'biologi'
+        ]);
+
+        // Kelas 12 Biologi
+
+        $this->create([
+            'course_name' => 'Hereditas',
+            'grade' => '12',
+            'category' => 'biologi'
+        ]);
+
+        $this->create([
+            'course_name' => 'Evolusi',
+            'grade' => '12',
+            'category' => 'biologi'
+        ]);
+
+        $this->create([
+            'course_name' => 'Genetika',
+            'grade' => '12',
+            'category' => 'biologi'
+        ]);
+
+        $this->create([
+            'course_name' => 'Bioteknologi',
+            'grade' => '12',
+            'category' => 'biologi'
+        ]);
+
         // Kelas 10 B Indonesia
 
         $this->create([
@@ -220,6 +487,58 @@ class Course extends Model
             'category' => 'Teks Eksposisi'
         ]);
 
+        // Kelas 11 B Indonesia
+
+        $this->create([
+            'course_name' => 'Teks Eksplanasi',
+            'grade' => '11',
+            'category' => 'indonesia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Prosedur',
+            'grade' => '11',
+            'category' => 'indonesia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Proposal',
+            'grade' => '11',
+            'category' => 'indonesia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Drama',
+            'grade' => '11',
+            'category' => 'indonesia'
+        ]);
+
+        // Kelas 12 B Indonesia
+
+        $this->create([
+            'course_name' => 'Novel',
+            'grade' => '12',
+            'category' => 'indonesia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Gagasan Utama',
+            'grade' => '12',
+            'category' => 'indonesia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Kritik dan Saran',
+            'grade' => '12',
+            'category' => 'indonesia'
+        ]);
+
+        $this->create([
+            'course_name' => 'Cerita Sejarah',
+            'grade' => '12',
+            'category' => 'indonesia'
+        ]);
+
         // Kelas 10 B Inggris
 
         $this->create([
@@ -249,6 +568,58 @@ class Course extends Model
         $this->create([
             'course_name' => 'Narrative Text',
             'grade' => '10',
+            'category' => 'inggris'
+        ]);
+
+        // Kelas 11 B Inggris
+
+        $this->create([
+            'course_name' => 'Opinion',
+            'grade' => '11',
+            'category' => 'inggris'
+        ]);
+
+        $this->create([
+            'course_name' => 'Expressing Emotions',
+            'grade' => '11',
+            'category' => 'inggris'
+        ]);
+
+        $this->create([
+            'course_name' => 'Text',
+            'grade' => '11',
+            'category' => 'inggris'
+        ]);
+
+        $this->create([
+            'course_name' => 'Grammar',
+            'grade' => '11',
+            'category' => 'inggris'
+        ]);
+
+        // Kelas 12 B Inggris
+
+        $this->create([
+            'course_name' => 'Procedure Text',
+            'grade' => '12',
+            'category' => 'inggris'
+        ]);
+
+        $this->create([
+            'course_name' => 'Suggestion',
+            'grade' => '12',
+            'category' => 'inggris'
+        ]);
+
+        $this->create([
+            'course_name' => 'Analytical Text',
+            'grade' => '12',
+            'category' => 'inggris'
+        ]);
+
+        $this->create([
+            'course_name' => 'Review Text',
+            'grade' => '12',
             'category' => 'inggris'
         ]);
     }
